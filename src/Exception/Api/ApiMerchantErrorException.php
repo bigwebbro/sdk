d0717879c7312c5a136474d09ccb227377e4559b
bigwebbro <bigwebbro@yandex.ru>
2025-10-30 00:24:51 +0700

    rector' fixes

--- src/Exception/Api/ApiMerchantErrorException.php
+++ src/Exception/Api/ApiMerchantErrorException.php
@@ -8,7 +8,7 @@ use Tiyn\MerchantApiSdk\Model\Error;
 
 class ApiMerchantErrorException extends \RuntimeException
 {
-    public function __construct(private Error $error, int $code, \Throwable $previous = null)
+    public function __construct(private readonly Error $error, int $code, \Throwable $previous = null)
     {
         parent::__construct($this->error->getMessage(), $code, $previous);
     }
