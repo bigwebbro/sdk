276c3030c4f971216774ed973349ce32ae8c3699
bigwebbro <bigwebbro@yandex.ru>
2025-10-30 14:47:18 +0700

    validation for CreateInvoicesRequest.php & precision hack for json float

---
+++ src/Configuration/Validation/ExpirationDateConstraintValidator.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tiyn\MerchantApiSdk\Configuration\Validation;
+
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Constraint;
+
+class ExpirationDateConstraintValidator extends ConstraintValidator
+{
+    public function validate(mixed $value, Constraint $constraint): void
+    {
+        if (!$constraint instanceof ExpirationDateConstraint) {
+            return;
+        }
+
+        if ($value <= new \DateTimeImmutable()) {
+            $this->context->buildViolation($constraint->message)
+                ->addViolation();
+        }
+    }
+}
