696c708135bc7934f82980034112f229c9f532de
bigwebbro <bigwebbro@yandex.ru>
2025-11-02 20:46:45 +0700

    added 5xx exception

---
+++ src/Exception/Service/ServiceUnavailableException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tiyn\MerchantApiSdk\Exception\Service;
+
+use Tiyn\MerchantApiSdk\Exception\Api\ApiMerchantErrorException;
+
+class ServiceUnavailableException extends \RuntimeException
+{
+
+}
