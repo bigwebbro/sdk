1d0c2f2ad2df2114080c3dfe7b95ac0134d5640c
bigwebbro <bigwebbro@yandex.ru>
2025-11-02 21:54:51 +0700

    datetime corner cases

--- tests/Configuration/SerializerFactoryTest.php
+++ tests/Configuration/SerializerFactoryTest.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Configuration;
 
+use Symfony\Component\Serializer\Normalizer\AbstractObjectNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 use Tiyn\MerchantApiSdk\Configuration\SerializerFactory;
 use PHPUnit\Framework\TestCase;
+use Tiyn\MerchantApiSdk\Model\Invoice\CreateInvoiceRequest;
 use Tiyn\MerchantApiSdk\Model\Invoice\CreateRefundResponse;
 use Tiyn\MerchantApiSdk\Model\Invoice\CreateRefundRequest;
 
@@ -96,5 +98,43 @@ class SerializerFactoryTest extends TestCase
             ['2444.444444', 2444.44],
             ['2444.445', 2444.45],
         ];
+    }
+
+    /**
+     * @test
+     */
+    public function SerializationDateTimeToString(): void
+    {
+        $dateTimeStr = '2025-11-09 11:08:24.909150+03:00';
+        $expectedJson = \sprintf('{"expirationDate":"%s"}', $dateTimeStr);
+        $date = \DateTimeImmutable::createFromFormat(SerializerFactory::DATE_TIME_FORMAT, $dateTimeStr);
+        $invoice = (new CreateInvoiceRequest())->setExpirationDate($date);
+        $result = $this->serializer->serialize(
+            $invoice,
+            'json',
+            [AbstractObjectNormalizer::SKIP_NULL_VALUES => true]
+        );
+
+        self::assertJsonStringEqualsJsonString($expectedJson, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function DeserializationStringToDateTime(): void
+    {
+        $dateTimeStr = '2025-11-09 11:08:24.909150+03:00';
+        $result = $this->serializer->deserialize(
+            \sprintf('{"expirationDate":"%s"}', $dateTimeStr),
+            CreateInvoiceRequest::class,
+            'json'
+        );
+
+        $property = (new \ReflectionClass($result))->getProperty('expirationDate');
+
+        self::assertEquals(
+            \DateTimeImmutable::createFromFormat(SerializerFactory::DATE_TIME_FORMAT, $dateTimeStr),
+            $property->getValue($result)
+        );
     }
 }
